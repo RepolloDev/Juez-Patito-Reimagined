ded61fb85ef40513f783e0d36f5912061c762dd9
repolloezzz <gabrielrepolloez@gmail.com>
2024-04-24 20:04:33 -0400

    Agregado de dependencia de tailwind y daisyui

---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+
+	darkMode: 'selector',
+
+	plugins: [require("daisyui")],
+	daisyui: {
+		themes: [
+			"retro",
+			"dracula",
+			"lofi",
+			"black"
+		]
+	}
+}
