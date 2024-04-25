dbb75356ed8e436adf547157e9375166030a1fc6
repolloezzz <gabrielrepolloez@gmail.com>
2024-04-24 20:16:14 -0400

    Agregado de fuentes de texto como dependencias

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,13 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				'geist': ['Geist Sans', 'sans-serif'],
+				'victor-mono': ['Victor Mono Variable', 'monospace'],
+				'major-mono': ['Major Mono Display', 'monospace'],
+			}
+		},
 	},
 
 	darkMode: 'selector',
