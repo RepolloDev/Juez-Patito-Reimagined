436f55009c51d678f2fcf91e73e6ca5876bb15ec
repolloezzz <gabrielrepolloez@gmail.com>
2024-05-01 22:04:53 -0400

    million.js is removed because it causes problems in components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from "@astrojs/react";
-import million from "million/compiler";
 
 import vercelServerless from "@astrojs/vercel/serverless";
 // import node from "@astrojs/node"
@@ -9,17 +8,10 @@ import vercelServerless from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  vite: {
-    plugins: [million.vite({
-      mode: 'react',
-      server: true,
-      auto: true,
-    })]
-  },
   compressHTML: true,
 
   // Output to Vercel Serverless
-  output: "server",
+  output: "hybrid",
   adapter: vercelServerless(
     {
       edgeMiddleware: true,
