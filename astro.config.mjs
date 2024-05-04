8f96b46c91101fc385aa94e8061f532c3abe2e4e
repolloezzz <gabrielrepolloez@gmail.com>
2024-05-04 15:20:30 -0400

    Cambio de provider

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,25 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from "@astrojs/react";
 
-import vercelServerless from "@astrojs/vercel/serverless";
-// import node from "@astrojs/node"
+//import vercelServerless from "@astrojs/vercel/serverless";
+//import node from "@astrojs/node";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react()]
 
   // Output to Vercel Serverless
-  output: "server",
-  adapter: vercelServerless(),
+  // output: "server",
+  // adapter: vercelServerless(),
 
   // Output to node server
   // output: "server",
   // adapter: node({
   //   mode: "middleware"
   // })
+  ,
+  output: "server",
+  adapter: netlify()
 });
