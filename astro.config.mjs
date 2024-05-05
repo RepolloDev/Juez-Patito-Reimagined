4b052378749aed6b18e3ae199f8b21dc3d4feae3
repolloezzz <gabrielrepolloez@gmail.com>
2024-05-05 17:35:35 -0400

    Arreglo de componente y tipos

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vercel from "@astrojs/vercel/serverless";
 import icon from "astro-icon";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
