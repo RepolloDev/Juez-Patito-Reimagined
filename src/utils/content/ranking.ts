1331b1b15b810c6ec6baa1de0d634f6994f047f5
repolloezzz <gabrielrepolloez@gmail.com>
2024-05-10 23:26:37 -0400

    Agregado de pagina de ranking

---
+++ src/utils/content/ranking.ts
@@ -0,0 +1,30 @@
+export interface UserRank {
+  user_id: string
+  nick: string
+  solved: number
+  submit: number
+}
+
+export async function getRakingList(): Promise<UserRank[]> {
+  const fetchURL = 'https://jv.umsa.bo/oj/ranklist.php?api=true'
+  const response = await fetch(fetchURL, { method: 'GET' })
+  if (response.ok) {
+    const { data } = await response.json()
+    return data as UserRank[]
+  }
+  return []
+}
+
+// Obtener una lista de 20 usuarios por pagina
+export async function getRankingPage(page: number): Promise<UserRank[]> {
+  const response = await getRakingList()
+  const start = (page - 1) * 100
+  const end = start + 100
+  return response.slice(start, end)
+}
+
+// Retornar el número de páginas
+export async function getNroRankingPages() {
+  const response = await getRakingList()
+  return Math.ceil(response.length / 100)
+}
